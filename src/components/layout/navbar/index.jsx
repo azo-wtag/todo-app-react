ea2919c4d888969f855abd902c698c9f57ffee46
azo-wtag <ahm.zobyer@welldev.io>
2023-03-20 10:51:24 +0600

    feat: navbar components elements added(without style)

--- src/components/layout/navbar/index.jsx
+++ src/components/layout/navbar/index.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Image from "../../base/image";
+import Styles from "./index.module.scss";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav
+      className={`container flex justify-between width-100 mx-auto ${Styles.navbar}`}
+    >
+      <div className="flex">
+        <Image src="logo.png" alt="logo" />
+        <h1>Todos</h1>
+      </div>
+
+      <div>
+        <Image src="search.png" alt="search" />
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
