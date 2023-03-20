fa03d290419cd3b7af414df871b027262f441d41
azo-wtag <ahm.zobyer@welldev.io>
2023-03-20 12:12:23 +0600

    feat: home container component added inside home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import HomeContainer from "../components/home";
 import Layout from "../components/layout";
 
 function Home() {
   return (
     <div>
       <Layout />
+      <HomeContainer />
     </div>
   );
 }
