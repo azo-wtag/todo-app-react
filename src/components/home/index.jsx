1d2ae756f379586a4b3bc832fddbe62bf0df0aac
azo-wtag <ahm.zobyer@welldev.io>
2023-03-20 12:12:05 +0600

    feat: home container component added

---
+++ src/components/home/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Button from "../base/button";
+import TaskCard from "../task";
+
+import Styles from "./index.module.scss";
+
+function HomeContainer() {
+  return (
+    <div className={`container mx-auto ${Styles.homeWrapper}`}>
+      <h1>Add Tasks</h1>
+      <div className={`flex justify-between ${Styles.buttonContainer}`}>
+        <Button>Create</Button>
+
+        <div>
+          <Button>All</Button>
+          <Button>Incomplete</Button>
+          <Button>Complete</Button>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-3 gap-34px">
+        <TaskCard />
+      </div>
+    </div>
+  );
+}
+
+export default HomeContainer;
