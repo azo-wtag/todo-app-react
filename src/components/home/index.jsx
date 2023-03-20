8cadcf56794c9bcc2915f5d75b9010d9828163c2
azo-wtag <ahm.zobyer@welldev.io>
2023-03-20 17:30:48 +0600

    feat:  create task, view task component integrated inside home container

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,15 +1,26 @@
-import React from "react";
-import Button from "../base/button";
-import TaskCard from "../task";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
+import Button from "../base/button";
+import CreateTask from "../task/create-new";
+import TaskCard from "../task/existing-card";
 import Styles from "./index.module.scss";
 
 function HomeContainer() {
+  const tasks = useSelector((state) => state.todo.tasks);
+  const [isNewTaskRequested, setIsNewTaskRequested] = useState(false);
+
+  const handleSuccessfulTaskEntry = () => {
+    setIsNewTaskRequested(false);
+  };
+
   return (
     <div className={`container mx-auto ${Styles.homeWrapper}`}>
       <h1>Add Tasks</h1>
       <div className={`flex justify-between ${Styles.buttonContainer}`}>
-        <Button>Create</Button>
+        <Button onButtonClick={() => setIsNewTaskRequested(true)}>
+          Create
+        </Button>
 
         <div>
           <Button>All</Button>
@@ -19,7 +30,19 @@ function HomeContainer() {
       </div>
 
       <div className="grid grid-cols-3 gap-34px">
-        <TaskCard />
+        {isNewTaskRequested && (
+          <CreateTask onSuccessfullTaskEntry={handleSuccessfulTaskEntry} />
+        )}
+
+        {tasks.map((task) => (
+          <TaskCard
+            key={task.id}
+            title={task.title}
+            createdAt={task.createdAt}
+            isCompleted={task.isCompleted}
+            completedAt={task.completedAt}
+          />
+        ))}
       </div>
 
       <div className="flex justify-center">
