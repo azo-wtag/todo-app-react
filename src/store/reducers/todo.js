43ef7a07ef2dd663bda05bcb5dbf2702bdd1bc7f
azo-wtag <ahm.zobyer@welldev.io>
2023-03-20 13:43:59 +0600

    config: tasks const added inside utils

--- src/store/reducers/todo.js
+++ src/store/reducers/todo.js
@@ -1,7 +1,8 @@
+import { tasks } from "../../utils/const";
 import { ADD_TASK } from "../constants/actionTypes";
 
 const initialTodoState = {
-  tasks: [],
+  tasks: tasks,
 };
 
 export const todoReducer = (state = initialTodoState, action) => {
