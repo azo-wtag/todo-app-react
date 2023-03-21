54bf60c5113b81e18db32f7b9a092183a479bb1c
azo-wtag <ahm.zobyer@welldev.io>
2023-03-21 12:42:55 +0600

    feat: delete task action & action types added

--- src/store/actions/todo.js
+++ src/store/actions/todo.js
@@ -1,8 +1,15 @@
-import { ADD_TASK } from "../constants/actionTypes";
+import { ADD_TASK, DELETE_TASK } from "../constants/actionTypes";
 
 export const addTaskToTodo = (task) => {
   return {
     type: ADD_TASK,
     payload: task,
+  };
+};
+
+export const deleteTaskFromTodo = (taskIndex) => {
+  return {
+    type: DELETE_TASK,
+    payload: taskIndex,
   };
 };
