e0924daf25f73409375af440f8a37498a460bd8d
azo-wtag <ahm.zobyer@welldev.io>
2023-03-23 17:12:29 +0600

    feat: edit task componet with functionality added

---
+++ src/components/task/existing-card/edit-task/index.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+import propTypes from "prop-types";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useDispatch } from "react-redux";
+import TextArea from "components/base/text-area";
+import Button from "components/base/button";
+import Image from "components/base/image";
+import {
+  TASK_TEXTAREA_NUM_OF_ROW,
+  CHECK_ICON_ALT_TAG,
+  CHECK_ICON_PATH,
+  DELETE_ICON_ALT_TAG,
+  DELETE_ICON_PATH,
+  TYPE_BUTTON,
+} from "utils/const";
+import { taskSchema } from "utils/schema";
+import { editTaskFromTodo, markTaskAsDone } from "store/actions/todo";
+
+function EditTaskForm({
+  taskIndex,
+  existingTitle,
+  onDeleteBtnClick,
+  onTaskEdit,
+}) {
+  const dispath = useDispatch();
+  useEffect(() => setValue("title", existingTitle), [existingTitle]);
+
+  const editTask = (task) => {
+    dispath(editTaskFromTodo({ index: taskIndex, title: task.title }));
+    setValue("title", "");
+    onTaskEdit();
+  };
+
+  const saveTaskAsDone = (task) => {
+    dispath(editTaskFromTodo({ index: taskIndex, title: task.title }));
+    dispath(markTaskAsDone(taskIndex));
+    setValue("title", "");
+    onTaskEdit();
+  };
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(taskSchema),
+  });
+
+  return (
+    <form>
+      <TextArea
+        numOfRows={TASK_TEXTAREA_NUM_OF_ROW}
+        register={{ ...register("title") }}
+        error={errors.title}
+      />
+
+      <div className="flex items-center">
+        <Button onButtonClick={handleSubmit(editTask)}>Save</Button>
+        <Button onButtonClick={handleSubmit(saveTaskAsDone)}>
+          <Image src={CHECK_ICON_PATH} alt={CHECK_ICON_ALT_TAG} />
+        </Button>
+        <Button buttonType={TYPE_BUTTON} onButtonClick={onDeleteBtnClick}>
+          <Image src={DELETE_ICON_PATH} alt={DELETE_ICON_ALT_TAG} />
+        </Button>
+      </div>
+    </form>
+  );
+}
+
+EditTaskForm.propTypes = {
+  taskIndex: propTypes.number.isRequired,
+  onDeleteBtnClick: propTypes.func.isRequired,
+  onTaskEdit: propTypes.func.isRequired,
+};
+
+export default EditTaskForm;
--- src/components/task/existing-card/index.jsx
+++ src/components/task/existing-card/index.jsx
@@ -5,12 +5,11 @@ import dayjs from "dayjs";
 import classnames from "classnames";
 
 import styles from "components/task/existing-card/index.module.scss";
-import TextArea from "components/base/text-area";
 import Button from "components/base/button";
-import { TASK_TEXTAREA_NUM_OF_ROW } from "utils/const";
-import { validateDayjsDate } from "utils/helper/validation";
 import ButtonContainer from "components/task/existing-card/button-container";
+import { validateDayjsDate } from "utils/helper/validation";
 import { deleteTaskFromTodo, markTaskAsDone } from "store/actions/todo";
+import EditTaskForm from "components/task/existing-card/edit-task";
 
 function TaskCard({
   indexNo,
@@ -18,6 +17,7 @@ function TaskCard({
   createdAt,
   isCompleted,
   isTaskOnEditMode,
+  completedAt,
 }) {
   const dispatch = useDispatch();
   const [isTextAreaVisible, setIsTextAreaVisible] = useState(isTaskOnEditMode);
@@ -27,29 +27,37 @@ function TaskCard({
 
   const taskHeaderClasses = classnames({ "text-line-thorught": isCompleted });
 
-  return (
-    <div className={styles.card}>
-      {isTextAreaVisible ? (
-        <TextArea numOfRows={TASK_TEXTAREA_NUM_OF_ROW} />
-      ) : (
-        <h3 className={taskHeaderClasses}>{title}</h3>
-      )}
-
-      <p className={styles.date}>created At: {formatDate(createdAt)}</p>
-
-      <div className="flex justify-between">
-        <ButtonContainer
-          onDoneButtonClick={() => dispatch(markTaskAsDone(indexNo))}
-          onEditButtonClick={() => setIsTextAreaVisible(true)}
-          onDeleteButtonClick={() => dispatch(deleteTaskFromTodo(indexNo))}
-          isTaskCompleted={isCompleted}
+  if (isTextAreaVisible) {
+    return (
+      <div className={styles.card}>
+        <EditTaskForm
+          taskIndex={indexNo}
+          existingTitle={title}
+          onDeleteBtnClick={() => setIsTextAreaVisible(false)}
+          onTaskEdit={() => setIsTextAreaVisible(false)}
         />
-        {isCompleted && (
-          <Button>Completed in {calculateDateDifference()} days</Button>
-        )}
       </div>
-    </div>
-  );
+    );
+  } else
+    return (
+      <div className={styles.card}>
+        <h3 className={taskHeaderClasses}>{title}</h3>
+        <p className={styles.date}>created At: {formatDate(createdAt)}</p>
+        <div className="flex justify-between">
+          <ButtonContainer
+            onDoneButtonClick={() => dispatch(markTaskAsDone(indexNo))}
+            onEditButtonClick={() => setIsTextAreaVisible(true)}
+            onDeleteButtonClick={() => dispatch(deleteTaskFromTodo(indexNo))}
+            isTaskCompleted={isCompleted}
+          />
+          {isCompleted && (
+            <Button>
+              Completed in {calculateDateDifference(completedAt)} days
+            </Button>
+          )}
+        </div>
+      </div>
+    );
 }
 
 TaskCard.propTypes = {
