dcfd073d6359bfbde3dc4cb61b2d6fb251bc6735
azo-wtag <ahm.zobyer@welldev.io>
2023-03-27 16:17:26 +0600

    feat: load more count logic on btn click added

--- src/components/task/load-more/index.jsx
+++ src/components/task/load-more/index.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import Button from "components/base/button";
 import styles from "./index.module.scss";
+import { loadMoreTask } from "store/actions/filter";
+import { CARD_PER_PAGE } from "utils/const";
 
 function LoadMoreBtnContainer() {
+  const dispatch = useDispatch();
+  const totalTaskCount = useSelector((state) => state.todo.tasks.length);
+  const visibleTaskCount = useSelector(
+    (state) => state.filter.visibleCardCount
+  );
+
+  const handleLoadMoreBtnClick = () => {
+    const nextPageCardCount =
+      (totalTaskCount - visibleTaskCount) % CARD_PER_PAGE;
+    if (nextPageCardCount >= 9) dispatch(loadMoreTask(9));
+    else dispatch(loadMoreTask(nextPageCardCount));
+  };
+
   return (
     <div className="flex justify-center">
-      <Button className={styles.loadMoreBtn}>Load More</Button>
+      <Button
+        className={styles.loadMoreBtn}
+        onButtonClick={handleLoadMoreBtnClick}
+      >
+        Load More
+      </Button>
     </div>
   );
 }
