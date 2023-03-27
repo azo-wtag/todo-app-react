41193758f5ca74f9a974cf0675b6a175891e13be
azo-wtag <ahm.zobyer@welldev.io>
2023-03-27 16:14:56 +0600

    feat: filter reducer combined

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
+import { filterReducer } from "./reducers/filter";
 import { todoReducer } from "./reducers/todo";
 
-const rootReducer = combineReducers({ todo: todoReducer });
+const rootReducer = combineReducers({
+  todo: todoReducer,
+  filter: filterReducer,
+});
 
 export default rootReducer;
