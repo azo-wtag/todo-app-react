8ddc9c7d72691fa6d6fbac15affc13aeaf933e88
azo-wtag <ahm.zobyer@welldev.io>
2023-03-28 13:29:40 +0600

    refactor: unnecessary style class removed

--- src/components/task/existing-task/button-container/index.jsx
+++ src/components/task/existing-task/button-container/index.jsx
@@ -14,16 +14,13 @@ import {
 function ButtonContainer({ onEditButtonClick }) {
   return (
     <div className="flex items-center">
-      <Button className={`${styles.button} ${styles.doneBtn}`}>
+      <Button className={styles.doneBtn}>
         <Image src={CHECK_ICON_PATH} alt={CHECK_ICON_ALT_TAG} />
       </Button>
-      <Button
-        className={`${styles.button} ${styles.editBtn}`}
-        onButtonClick={onEditButtonClick}
-      >
+      <Button className={styles.editBtn} onButtonClick={onEditButtonClick}>
         <Image src={EDIT_ICON_PATH} alt={EDIT_ICON_ALT_TAG} />
       </Button>
-      <Button className={`${styles.button} ${styles.deleteBtn}`}>
+      <Button className={styles.deleteBtn}>
         <Image src={DELETE_ICON_PATH} alt={DELETE_ICON_ALT_TAG} />
       </Button>
     </div>
