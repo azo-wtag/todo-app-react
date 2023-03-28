e08a3e0788e2b1fb70e81a41c5c9313454f5ec06
azo-wtag <ahm.zobyer@welldev.io>
2023-03-28 12:55:41 +0600

    refactor: form button types stored in const

--- src/utils/const/formElements.js
+++ src/utils/const/formElements.js
@@ -2,3 +2,6 @@ export const TASK_TEXTAREA_NUM_OF_ROW = 4;
 export const TEXTAREA_DEFAULT_ROW = 4;
 
 export const TITLE_FIELD_NAME_ATTRIBUTE = "title";
+
+export const TYPE_SUBMIT = "submit";
+export const TYPE_BUTTON = "button";
