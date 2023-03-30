bdab34481dadadaeb349eddbcb357b3509d14c12
azo-wtag <ahm.zobyer@welldev.io>
2023-03-30 13:25:09 +0600

    feat: filter state updated on filter btns click

--- src/components/filter-btn-container/index.jsx
+++ src/components/filter-btn-container/index.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import Button from "components/base/button";
+import { useDispatch } from "react-redux";
+import { filterTask } from "store/actions/filter";
+import {
+  TASK_FILTER_ALL,
+  TASK_FILTER_COMPLETED,
+  TASK_FILTER_INCOMPLETED,
+} from "utils/const";
 
 function FilterBtnContainer() {
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <Button>All</Button>
-      <Button>Incomplete</Button>
-      <Button>Complete</Button>
+      <Button onButtonClick={() => dispatch(filterTask(TASK_FILTER_ALL))}>
+        All
+      </Button>
+      <Button
+        onButtonClick={() => dispatch(filterTask(TASK_FILTER_INCOMPLETED))}
+      >
+        Incomplete
+      </Button>
+      <Button onButtonClick={() => dispatch(filterTask(TASK_FILTER_COMPLETED))}>
+        Complete
+      </Button>
     </div>
   );
 }
