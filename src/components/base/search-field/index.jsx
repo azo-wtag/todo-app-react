fd852893cdcbaa9efaff467a6ba8f77022759a95
azo-wtag <ahm.zobyer@welldev.io>
2023-04-02 15:43:35 +0600

    feat: search field form register with react-from-hook

--- src/components/base/search-field/index.jsx
+++ src/components/base/search-field/index.jsx
@@ -1,14 +1,41 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import InputField from "components/base/input/input";
 import Image from "components/base/image";
-import { SEARCH_ICON_ALT_TAG, SEARCH_ICON_PATH } from "utils/const";
+import {
+  FORM_VALIDATION_MODE_ONCHANGE,
+  SEARCH_ICON_ALT_TAG,
+  SEARCH_ICON_PATH,
+  TITLE_FIELD_NAME_ATTRIBUTE,
+} from "utils/const";
+import { taskSchema } from "utils/schema";
 
 function SearchField() {
+  const searchTaskByTitle = (task) => {
+    console.log(task);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: FORM_VALIDATION_MODE_ONCHANGE,
+    resolver: yupResolver(taskSchema),
+  });
+
   return (
-    <div className="flex items-center">
-      <InputField />
+    <form
+      className="flex items-center"
+      onSubmit={handleSubmit(searchTaskByTitle)}
+    >
+      <InputField
+        register={register(TITLE_FIELD_NAME_ATTRIBUTE)}
+        error={errors.title}
+      />
       <Image src={SEARCH_ICON_PATH} alt={SEARCH_ICON_ALT_TAG} />
-    </div>
+    </form>
   );
 }
 
