c3ed8a582c0c352dcdaf09a3731a3aaff5435759
azo-wtag <ahm.zobyer@welldev.io>
2023-04-02 15:28:23 +0600

    feat: search field component init

---
+++ src/components/base/search-field/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import InputField from "components/base/input/input";
+import Image from "components/base/image";
+import { SEARCH_ICON_ALT_TAG, SEARCH_ICON_PATH } from "utils/const";
+
+function SearchField() {
+  return (
+    <div className="">
+      <InputField />
+      <Image src={SEARCH_ICON_PATH} alt={SEARCH_ICON_ALT_TAG} />
+    </div>
+  );
+}
+
+export default SearchField;
