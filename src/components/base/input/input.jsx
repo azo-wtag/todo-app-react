fe9db0cba8cda284413a4ea33c3c6934884de458
azo-wtag <ahm.zobyer@welldev.io>
2023-04-02 15:35:56 +0600

    feat: basic props decalred & validated in input field

--- src/components/base/input/input.jsx
+++ src/components/base/input/input.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import propTypes from "prop-types";
 
-function InputField() {
-  return <input type="text" />;
+function InputField({ classNames, type, register, error }) {
+  return (
+    <div>
+      <input type={type} className={classNames} {...register} />{" "}
+      {error && <p>{error.message}</p>}
+    </div>
+  );
 }
+
+InputField.propTypes = {
+  classNames: propTypes.string,
+  type: propTypes.string,
+  register: propTypes.object.isRequired,
+  error: propTypes.object,
+};
+
+InputField.defaultProps = {
+  classNames: "",
+  type: "text",
+};
 
 export default InputField;
