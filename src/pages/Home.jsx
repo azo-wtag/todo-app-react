d2c643d65bbcfee5f6bb43f2c7cc8679ca0f91dd
azo-wtag <ahm.zobyer@welldev.io>
2023-04-04 14:40:20 +0600

    feat: toast container component added inside home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import HomeContainer from "components/home-container";
 import Layout from "components/layout";
 
 function Home() {
   return (
     <>
+      <ToastContainer />
       <Layout />
       <HomeContainer />
     </>
