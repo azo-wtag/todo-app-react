cc60d3acab7e411a38168d4acc9a5c77524ed0d7
azo-wtag <ahm.zobyer@welldev.io>
2023-04-04 15:08:41 +0600

    feat: errortask mesaage triggered on delete btn click

--- src/components/task/existing-task/task-card/index.jsx
+++ src/components/task/existing-task/task-card/index.jsx
@@ -8,10 +8,11 @@ import styles from "components/task/existing-task/task-card/index.module.scss";
 import ButtonContainer from "components/task/existing-task/button-container";
 import Button from "components/base/button";
 import EditTaskForm from "components/task/existing-task/edit-task";
-import { TASK_DATE_FORMAT } from "utils/const";
+import { TASK_DATE_FORMAT, TASK_DELETED_SUCCESS_MESSAGE } from "utils/const";
 import { validateDayjsDate } from "utils/helper/validation";
 import { deleteTaskFromTodo, markTaskAsDone } from "store/actions/todo";
 import { decreaseNumOfVisibleTasks } from "store/actions/filter";
+import { showErrorToast } from "utils/toast";
 
 function TaskCard({
   taskId,
@@ -37,6 +38,7 @@ function TaskCard({
   );
   const handleDeleButtonClick = () => {
     dispatch(deleteTaskFromTodo(taskId));
+    showErrorToast(TASK_DELETED_SUCCESS_MESSAGE);
     if (tasks.length === numOfCardVisible)
       dispatch(decreaseNumOfVisibleTasks());
   };
