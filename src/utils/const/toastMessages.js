e18807c4b1e4bedd866a7a5948939698b03d4286
azo-wtag <ahm.zobyer@welldev.io>
2023-04-04 14:39:09 +0600

    feat: error messages defined inside const

--- src/utils/const/toastMessages.js
+++ src/utils/const/toastMessages.js
@@ -1,2 +1,7 @@
 export const CUSTOM_ERROR_MESSAGE_TYPE = "custom";
 export const TASK_TITLE_ERROR_MESSAGE = "Please enter valid a task";
+
+export const TASK_ADDED_SUCCESS_MESSAGE = "Task added successfully";
+export const TASK_DELETED_SUCCESS_MESSAGE = "Task deleted successfully";
+export const SOMETHING_GOES_WRONG_MESSAGE =
+  "Something went wrong please try again";
