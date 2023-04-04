e551307edf9b65eeed60c445e00658dc1a04c3e7
azo-wtag <ahm.zobyer@welldev.io>
2023-04-04 14:15:52 +0600

    feat: white check icon path added inside const

--- src/utils/const/images.js
+++ src/utils/const/images.js
@@ -3,6 +3,7 @@ import searchIcon from "assets/images/search.png";
 import deleteIcon from "assets/images/delete.png";
 import editIcon from "assets/images/edit.png";
 import checkIcon from "assets/images/check.png";
+import checkIconBgWhite from "assets/images/check-white.png";
 import noTaskFoundIcon from "assets/images/no-task-found.png";
 
 export const LOGO_ALT_TAG = "todo app logo";
@@ -17,4 +18,5 @@ export const SEARCH_ICON_PATH = searchIcon;
 export const DELETE_ICON_PATH = deleteIcon;
 export const EDIT_ICON_PATH = editIcon;
 export const CHECK_ICON_PATH = checkIcon;
+export const CHECK_ICON_WHITE_PATH = checkIconBgWhite;
 export const NO_TASK_FOUND_ICON_PATH = noTaskFoundIcon;
