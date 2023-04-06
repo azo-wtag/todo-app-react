4e14e626332e22a6b0cc239ede11c4d5b3c5a467
azo-wtag <ahm.zobyer@welldev.io>
2023-04-06 11:41:17 +0600

    feat: button is Disabled prop added

--- src/components/base/button/index.jsx
+++ src/components/base/button/index.jsx
@@ -3,12 +3,19 @@ import propTypes from "prop-types";
 import styles from "components/base/button/index.module.scss";
 import { TYPE_SUBMIT } from "utils/const";
 
-function Button({ children, onButtonClick, className, buttonType }) {
+function Button({
+  children,
+  onButtonClick,
+  className,
+  buttonType,
+  isDisabled,
+}) {
   return (
     <button
       className={`${styles.button} ${className}`}
       onClick={onButtonClick}
       type={buttonType}
+      disabled={isDisabled}
     >
       {children}
     </button>
@@ -20,12 +27,14 @@ Button.propTypes = {
   className: propTypes.string,
   onButtonClick: propTypes.func,
   buttonType: propTypes.string,
+  isDisabled: propTypes.bool,
 };
 
 Button.defaultProps = {
   className: "",
   onButtonClick: () => {},
   buttonType: TYPE_SUBMIT,
+  isDisabled: false,
 };
 
 export default Button;
