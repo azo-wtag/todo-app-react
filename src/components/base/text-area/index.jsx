5ae4c8615be435ead0c2012688ece7421f7887a8
azo-wtag <ahm.zobyer@welldev.io>
2023-04-09 12:59:29 +0600

    feat: text area isDisable default props set from redux

--- src/components/base/text-area/index.jsx
+++ src/components/base/text-area/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import propTypes from "prop-types";
+import { connect } from "react-redux";
 import { TEXTAREA_DEFAULT_ROW } from "utils/const";
 
-function TextArea({ numOfRows, register, error }) {
+function TextArea({ numOfRows, register, error, isDisabled }) {
   return (
     <div>
       <textarea
@@ -10,6 +11,7 @@ function TextArea({ numOfRows, register, error }) {
         {...register}
         className="width-full"
         rows={numOfRows}
+        disabled={isDisabled}
       ></textarea>
       {error && <p>{error.message}</p>}
     </div>
@@ -20,10 +22,15 @@ TextArea.propTypes = {
   noOfRows: propTypes.number,
   register: propTypes.object.isRequired,
   error: propTypes.object,
+  isDisabled: propTypes.bool,
 };
+
+const mapStateToProps = (state) => ({
+  isDisabled: state.filter.isFiltering,
+});
 
 TextArea.defaultProps = {
   noOfRows: TEXTAREA_DEFAULT_ROW,
 };
 
-export default TextArea;
+export default connect(mapStateToProps)(TextArea);
