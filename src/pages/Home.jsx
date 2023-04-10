1c83a976a82ec793a8b68ee1b4eb31e9a908800d
azo-wtag <ahm.zobyer@welldev.io>
2023-04-10 18:10:07 +0600

    feat: supabase configured

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import HomeContainer from "components/home-container";
 import Layout from "components/layout";
+import supabase from "config";
 
 function Home() {
+  useEffect(() => {
+    async function fetchTasks() {
+      const { data } = await supabase.from("tasks").select();
+      console.log(data);
+    }
+
+    fetchTasks();
+  }, []);
+
   return (
     <>
       <ToastContainer />
