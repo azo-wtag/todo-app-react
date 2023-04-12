284ea50f002a40e32c41235d33ddd813ed5c2f4f
azo-wtag <ahm.zobyer@welldev.io>
2023-04-12 17:30:32 +0600

    feat: loader is visible on page load based on isLoading global state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,26 @@ import "react-toastify/dist/ReactToastify.css";
 import HomeContainer from "components/home-container";
 import Layout from "components/layout";
 import supabase from "config";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadTaskFromDB } from "store/actions/todo";
 import { showErrorToast } from "utils/toast";
+import Loader from "components/base/loader";
+import { setisLoading } from "store/actions/filter";
 
 function Home() {
   const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.filter.isLoading);
+
   useEffect(() => {
     async function fetchTasks() {
+      dispatch(setisLoading(true));
       try {
         const { data } = await supabase.from("tasks").select();
         dispatch(loadTaskFromDB(data));
       } catch (error) {
         showErrorToast(error.message);
       }
+      dispatch(setisLoading(false));
     }
 
     fetchTasks();
@@ -25,6 +31,7 @@ function Home() {
 
   return (
     <>
+      {isLoading && <Loader />}
       <ToastContainer />
       <Layout />
       <HomeContainer />
