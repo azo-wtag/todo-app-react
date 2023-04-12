14db93c8218e68e63620b3eae3f046a0a417bbab
azo-wtag <ahm.zobyer@welldev.io>
2023-04-12 15:12:11 +0600

    refactor: date difference calculation func moved to helper file

--- src/components/task/existing-task/task-card/index.jsx
+++ src/components/task/existing-task/task-card/index.jsx
@@ -11,6 +11,7 @@ import Button from "components/base/button";
 import { TASK_DATE_FORMAT, TASK_TEXTAREA_NUM_OF_ROW } from "utils/const";
 import { validateDayjsDate } from "utils/helper/validation";
 import { deleteTask, markAsDone } from "store/actions/todo";
+import { calculateDateDifference } from "utils/helper";
 
 function TaskCard({
   taskId,
@@ -25,12 +26,6 @@ function TaskCard({
   const [isTextAreaVisible, setIsTextAreaVisible] = useState(isTaskOnEditMode);
   const formatDate = (date) =>
     dayjs(date, TASK_DATE_FORMAT).format(TASK_DATE_FORMAT);
-  const calculateDateDifference = (completedAt, createdAt) => {
-    const dateDifference = dayjs(completedAt).diff(createdAt, "day");
-    return dateDifference === 0
-      ? `1 day`
-      : `${Math.abs(dateDifference) + 1} days`;
-  };
 
   const taskHeaderClasses = classnames({ "text-line-through": isCompleted });
 
