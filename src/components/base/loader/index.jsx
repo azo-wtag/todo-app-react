143ad682546bcbd73a6c8646ca9ffb2931c08ee4
azo-wtag <ahm.zobyer@welldev.io>
2023-04-13 10:56:00 +0600

    feat: loader image style class received as props

--- src/components/base/loader/index.jsx
+++ src/components/base/loader/index.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import propTypes from "prop-types";
+
 import Image from "components/base/image";
 import { LOADER_ALT_TAG, LOADER_IMG_PATH } from "utils/const";
 import styles from "components/base/loader/index.module.scss";
 
-function Loader() {
+function Loader({ imageClassName }) {
   return (
     <div className={styles.loader}>
       <Image
         src={LOADER_IMG_PATH}
         alt={LOADER_ALT_TAG}
-        className={styles.icon}
+        className={`${styles.icon} ${imageClassName}`}
       />
     </div>
   );
 }
+
+Loader.propTypes = {
+  imageClassName: propTypes.string,
+};
+
+Loader.defaultProps = {
+  imageClassName: "",
+};
 
 export default Loader;
