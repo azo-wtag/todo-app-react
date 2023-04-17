73a28f246003646c0933f1b7fea8ef35fb02fe25
azo-wtag <ahm.zobyer@welldev.io>
2023-04-17 11:38:49 +0600

    refactor: Todo app logo & alt tag const name

--- src/utils/const/images.js
+++ src/utils/const/images.js
@@ -4,13 +4,13 @@ import deleteIcon from "assets/images/delete.png";
 import editIcon from "assets/images/edit.png";
 import checkIcon from "assets/images/check.png";
 
-export const ALT_LOGO_ALT_TAG = "todo app logo";
+export const ALT_LOGO_TAG = "todo app logo";
 export const ALT_SEARCH_ICON_TAG = "task search icon";
 export const ALT_DELETE_ICON_TAG = "delete task button icon";
 export const ALT_EDIT_ICON_TAG = "edit task button icon";
 export const ALT_CHECK_ICON_TAG = "complete task button icon";
 
-export const ALT_LOGO_IMG_TAG = logoImage;
+export const PATH_LOGO_ICON = logoImage;
 export const PATH_SEARCH_ICON = searchIcon;
 export const PATH_DELETE_ICON = deleteIcon;
 export const PATH_EDIT_ICON = editIcon;
