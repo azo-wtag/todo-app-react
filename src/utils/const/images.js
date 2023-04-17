b1b00165e4e8aaf49015570b99fd0d778c12608d
azo-wtag <ahm.zobyer@welldev.io>
2023-04-17 11:14:43 +0600

    refactor: const name for path & alt tag

--- src/utils/const/images.js
+++ src/utils/const/images.js
@@ -4,14 +4,14 @@ import deleteIcon from "assets/images/delete.png";
 import editIcon from "assets/images/edit.png";
 import checkIcon from "assets/images/check.png";
 
-export const LOGO_ALT_TAG = "todo app logo";
-export const SEARCH_ICON_ALT_TAG = "task search icon";
-export const DELETE_ICON_ALT_TAG = "delete task button icon";
-export const EDIT_ICON_ALT_TAG = "edit task button icon";
-export const CHECK_ICON_ALT_TAG = "complete task button icon";
+export const ALT_LOGO_ALT_TAG = "todo app logo";
+export const ALT_SEARCH_ICON_TAG = "task search icon";
+export const ALT_DELETE_ICON_TAG = "delete task button icon";
+export const ALT_EDIT_ICON_TAG = "edit task button icon";
+export const ALT_CHECK_ICON_TAG = "complete task button icon";
 
-export const LOGO_IMG_PATH = logoImage;
-export const SEARCH_ICON_PATH = searchIcon;
-export const DELETE_ICON_PATH = deleteIcon;
-export const EDIT_ICON_PATH = editIcon;
-export const CHECK_ICON_PATH = checkIcon;
+export const ALT_LOGO_IMG_TAG = logoImage;
+export const PATH_SEARCH_ICON = searchIcon;
+export const PATH_DELETE_ICON = deleteIcon;
+export const PATH_EDIT_ICON = editIcon;
+export const PATH_CHECK_ICON = checkIcon;
