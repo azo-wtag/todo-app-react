e932aba7eb2b837d18f29144e9703efa5227547a
azo-wtag <ahm.zobyer@welldev.io>
2023-04-18 12:33:07 +0600

    refactor: loader image name & alt reimported

--- src/components/base/loader/index.jsx
+++ src/components/base/loader/index.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import propTypes from "prop-types";
 
 import Image from "components/base/image";
-import { LOADER_ALT_TAG, LOADER_IMG_PATH } from "utils/const";
+import { ALT_LOADER_TAG, PATH_LOADER_ICON } from "utils/const";
 import styles from "components/base/loader/index.module.scss";
 
 function Loader({ imageClassName }) {
   return (
     <div className={styles.loader}>
       <Image
-        src={LOADER_IMG_PATH}
-        alt={LOADER_ALT_TAG}
+        src={PATH_LOADER_ICON}
+        alt={ALT_LOADER_TAG}
         className={`${styles.icon} ${imageClassName}`}
       />
     </div>
