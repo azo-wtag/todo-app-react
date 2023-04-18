3366b14cefd4a50304af81059ef649223543c3bd
azo-wtag <ahm.zobyer@welldev.io>
2023-04-18 11:28:36 +0600

    refactor: updated plus icon const imported

--- src/components/home-container/index.jsx
+++ src/components/home-container/index.jsx
@@ -9,8 +9,8 @@ import LoadMoreBtnContainer from "components/load-more-btn-container";
 import ExistingTaskCardContaienr from "components/task/existing-task/container";
 import Image from "components/base/image";
 import {
-  PLUS_ICON_ALT_TAG,
-  PLUS_ICON_PATH,
+  ALT_PLUS_ICON_TAG,
+  PATH_PLUS_ICON,
   TASK_FILTER_COMPLETED,
   TASK_FILTER_INCOMPLETED,
 } from "utils/const";
@@ -60,7 +60,7 @@ function HomeContainer() {
             onClick={() => setIsNewTaskRequested(true)}
             className={`fw-500 flex items-center justify-center ${styles.createTaskBtn}`}
           >
-            <Image src={PLUS_ICON_PATH} alt={PLUS_ICON_ALT_TAG} />
+            <Image src={PATH_PLUS_ICON} alt={ALT_PLUS_ICON_TAG} />
             <span className={styles.createTxt}>Create</span>
           </Button>
           <FilterBtnContainer />
