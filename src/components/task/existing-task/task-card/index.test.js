ac24680c3e7747f9498a17c6b2fce0444e5520e9
“azo-wtag” <ahm.zobyer@welldev.io>
2023-04-20 00:08:34 +0600

    test: button visibility for completed task

---
+++ src/components/task/existing-task/task-card/index.test.js
@@ -0,0 +1,60 @@
+import { act, screen } from "@testing-library/react";
+import { createClient } from "@supabase/supabase-js";
+import userEvent from "@testing-library/user-event";
+import renderConnected from "utils/helper/rendererConnected";
+import TaskCard from "../task-card";
+import { ToastContainer } from "react-toastify";
+
+jest.mock("@supabase/supabase-js", () => ({
+  createClient: jest.fn(),
+}));
+
+const mockInsert = jest.fn();
+const mockSupabase = {
+  from: () => ({
+    insert: mockInsert.mockReturnValue({ data: [{ id: 1 }] }),
+  }),
+};
+createClient.mockReturnValue(mockSupabase);
+
+describe("<TaskCard />", () => {
+  beforeEach(() => {
+    renderConnected(
+      <>
+        <ToastContainer />
+        <TaskCard
+          taskId="#123"
+          title="Review a PR"
+          createdAt="2023-04-11"
+          isCompleted={true}
+          completedAt={"2023-04-12"}
+        />
+      </>
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("should match date difference between task creation & completion time", () => {
+    const completedDaysBtn = screen.getByRole("button", {
+      name: /completed in/i,
+    });
+    expect(completedDaysBtn).toHaveTextContent(/completed in 2 days/i);
+  });
+
+  test("should remove task on delete button click", async () => {
+    const user = userEvent.setup();
+
+    const deleteButton = screen.getByRole("button", {
+      name: /delete task button icon/i,
+    });
+    expect(deleteButton).toBeInTheDocument();
+    await act(async () => {
+      await user.click(deleteButton);
+    });
+    const deleteMsg = screen.getByText(/task deleted successfully/i);
+    expect(deleteMsg).toBeInTheDocument();
+  });
+});
