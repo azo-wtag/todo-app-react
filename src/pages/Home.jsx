09a70fc45d5c5c1ed71e535bd1e7cf7688771464
azo-wtag <ahm.zobyer@welldev.io>
2023-04-26 18:29:51 +0600

    feat: max num of toast will be visible at a time is 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,12 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import HomeContainer from "components/home-container";
 import Layout from "components/layout";
+import { NUM_OF_CONCURRENT_TOAST } from "utils/const/toast";
 
 function Home() {
   return (
     <>
-      <ToastContainer />
+      <ToastContainer limit={NUM_OF_CONCURRENT_TOAST} />
       <Layout />
       <HomeContainer />
     </>
