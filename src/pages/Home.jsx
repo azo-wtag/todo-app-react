a19de1d0186478f739d37eae5cbb14f66c92a666
azo-wtag <ahm.zobyer@welldev.io>
2023-04-27 09:58:22 +0600

    refactor: total toast limit removed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,11 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import HomeContainer from "components/home-container";
 import Layout from "components/layout";
-import { NUM_OF_CONCURRENT_TOAST } from "utils/const/toast";
 
 function Home() {
   return (
     <>
-      <ToastContainer limit={NUM_OF_CONCURRENT_TOAST} />
+      <ToastContainer />
       <Layout />
       <HomeContainer />
     </>
