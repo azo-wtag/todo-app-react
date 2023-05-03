8a206cb1e376e3ba70e38da93164ade2c25c795e
azo-wtag <ahm.zobyer@welldev.io>
2023-05-03 16:08:40 +0600

    fix: created at date formatter func

--- src/components/task/existing-task/task-card/index.jsx
+++ src/components/task/existing-task/task-card/index.jsx
@@ -12,7 +12,7 @@ function TaskCard({ createdAt, isCompleted, isTaskOnEditMode, title }) {
   const [isTextAreaVisible, setIsTextAreaVisible] = useState(isTaskOnEditMode);
 
   function formatDate(date) {
-    dayjs(date).format(TASK_DATE_FORMAT);
+    return dayjs(date).format(TASK_DATE_FORMAT);
   }
 
   function handleEditClick() {
