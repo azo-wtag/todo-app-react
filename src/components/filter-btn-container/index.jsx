3ae213ee769a6adead616c4828e59667bec7cfbf
azo-wtag <ahm.zobyer@welldev.io>
2023-05-03 09:33:27 +0600

    feat: selected filter btn style added

--- src/components/filter-btn-container/index.jsx
+++ src/components/filter-btn-container/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "components/base/button";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import classnames from "classnames";
 import styles from "components/filter-btn-container/index.module.scss";
 import { filterTask } from "store/actions/filter";
 import {
@@ -11,24 +12,46 @@ import {
 
 function FilterBtnContainer() {
   const dispatch = useDispatch();
+  const filterState = useSelector((state) => state.filter.filteredCardState);
+
+  const allBtnClassNames = classnames(
+    "bg-white",
+    "text-grey-ship",
+    styles.allBtn,
+    { [styles.selectedBtn]: filterState == TASK_FILTER_ALL }
+  );
+
+  const incompleteBtnClassNames = classnames(
+    "bg-white",
+    "text-grey-ship",
+    styles.incompleteBtn,
+    { [styles.selectedBtn]: filterState == TASK_FILTER_INCOMPLETED }
+  );
+
+  const completeBtnClassNames = classnames(
+    "bg-white",
+    "text-grey-ship",
+    styles.completeBtn,
+    { [styles.selectedBtn]: filterState == TASK_FILTER_COMPLETED }
+  );
 
   return (
     <div className={styles.buttonContainer}>
       <Button
         onClick={() => dispatch(filterTask(TASK_FILTER_ALL))}
-        className={`bg-white ${styles.allBtn}`}
+        className={allBtnClassNames}
       >
         All
       </Button>
       <Button
         onClick={() => dispatch(filterTask(TASK_FILTER_INCOMPLETED))}
-        className={`bg-white ${styles.incompleteBtn}`}
+        className={incompleteBtnClassNames}
       >
         Incomplete
       </Button>
       <Button
         onClick={() => dispatch(filterTask(TASK_FILTER_COMPLETED))}
-        className={`bg-white ${styles.completeBtn}`}
+        className={completeBtnClassNames}
       >
         Complete
       </Button>
