8cf145bbb28de1b8cefd37cab4517c95d2a4fddf
azo-wtag <ahm.zobyer@welldev.io>
2023-05-10 14:52:52 +0600

    refactor: import order inside existing-task-card component

--- src/components/task/existing-task/task-card/index.jsx
+++ src/components/task/existing-task/task-card/index.jsx
@@ -5,12 +5,12 @@ import dayjs from "dayjs";
 import classnames from "classnames";
 import styles from "components/task/existing-task/task-card/index.module.scss";
 import Button from "components/base/button";
+import EditTaskForm from "components/task/existing-task/edit-task";
+import ButtonContainer from "components/task/existing-task/button-container";
 import { TASK_DATE_FORMAT } from "utils/const";
 import { validateDayjsDate } from "utils/helper/validation";
-import ButtonContainer from "components/task/existing-task/button-container";
-import { deleteTask, markAsDone } from "store/actions/todo";
-import EditTaskForm from "components/task/existing-task/edit-task";
 import { calculateDateDifference } from "utils/helper";
+import { deleteTask, markAsDone } from "store/actions/todo";
 
 function TaskCard({
   createdAt,
