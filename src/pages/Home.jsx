9eaae324a7ac5e80655d9496d6312069e53337e9
azo-wtag <ahm.zobyer@welldev.io>
2023-05-14 14:02:06 +0600

    feat: is loading props is passed to <Loader /> from Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useDispatch, useSelector } from "react-redux";
 import HomeContainer from "components/home-container";
 import Layout from "components/layout";
 import Loader from "components/base/loader";
 import supabase from "config";
-import { useDispatch } from "react-redux";
 import { loadTaskFromDB } from "store/actions/todo";
+import { setisLoading } from "store/actions/filter";
 import { showErrorToast } from "utils/toast";
 
 function Home() {
   const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.filter.isLoading);
 
   useEffect(() => {
     async function fetchTasks() {
+      dispatch(setisLoading(true));
       try {
         const { data } = await supabase.from("tasks").select();
         dispatch(loadTaskFromDB(data));
       } catch (error) {
         showErrorToast(error.message);
       }
+      dispatch(setisLoading(false));
     }
 
     fetchTasks();
@@ -27,7 +31,7 @@ function Home() {
 
   return (
     <>
-      <Loader />
+      <Loader isLoading={isLoading} />
       <ToastContainer />
       <Layout />
       <HomeContainer />
