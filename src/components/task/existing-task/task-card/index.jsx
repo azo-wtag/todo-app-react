6c1ae40204189854a98ac21036a44709b97748e9
azo-wtag <ahm.zobyer@welldev.io>
2023-05-15 16:14:50 +0600

    feat: toast message const import

--- src/components/task/existing-task/task-card/index.jsx
+++ src/components/task/existing-task/task-card/index.jsx
@@ -7,7 +7,12 @@ import styles from "components/task/existing-task/task-card/index.module.scss";
 import Button from "components/base/button";
 import EditTaskForm from "components/task/existing-task/edit-task";
 import ButtonContainer from "components/task/existing-task/button-container";
-import { SINGLE_CARD, TASK_DATE_FORMAT } from "utils/const";
+import {
+  SINGLE_CARD,
+  SUCCESS_MESSAGE_TASK_DELETED,
+  SUCCESS_MESSAGE_TASK_DONE,
+  TASK_DATE_FORMAT,
+} from "utils/const";
 import { validateDayjsDate } from "utils/helper/validation";
 import { deleteTask, markAsDone } from "store/slices/todoSlce";
 import { calculateDateDifference } from "utils/helper";
