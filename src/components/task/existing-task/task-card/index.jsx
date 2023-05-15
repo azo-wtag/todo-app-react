a0366af0b87cdfc2c3376ab0aba8f83175738e5b
azo-wtag <ahm.zobyer@welldev.io>
2023-05-15 13:45:49 +0600

    feat: dispatch markTaskDone action from RTK actions

--- src/components/task/existing-task/task-card/index.jsx
+++ src/components/task/existing-task/task-card/index.jsx
@@ -9,7 +9,7 @@ import Button from "components/base/button";
 import { TASK_DATE_FORMAT } from "utils/const";
 import { validateDayjsDate } from "utils/helper/validation";
 import ButtonContainer from "components/task/existing-task/button-container";
-import { deleteTask } from "store/slices/todoSlce";
+import { deleteTask, markAsDone } from "store/slices/todoSlce";
 import { calculateDateDifference } from "utils/helper";
 
 function TaskCard({
