0c37db52a2b095634c942a12d438ca170741278b
azo-wtag <ahm.zobyer@welldev.io>
2023-05-17 17:04:57 +0600

    feat: delete event added inside object

--- src/components/task/existing-task/button-group/index.jsx
+++ src/components/task/existing-task/button-group/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
+import propTypes from "prop-types";
 import styles from "components/task/existing-task/button-group/index.module.scss";
 import Button from "components/base/button";
 import Image from "components/base/image";
@@ -12,37 +13,48 @@ import {
   ICON_EDIT,
 } from "utils/const";
 
-function ButtonGroup() {
+function ButtonGroup({ onDeleteClick }) {
   const actionButtons = [
     {
       id: uuidv4(),
       styleClass: styles.doneBtn,
       src: ICON_CHECK,
       alt: ALT_TAG_ICON_CHECK,
+      onClick: () => {},
     },
     {
       id: uuidv4(),
       styleClass: styles.editBtn,
       src: ICON_EDIT,
       alt: ALT_TAG_ICON_EDIT,
+      onClick: () => {},
     },
     {
       id: uuidv4(),
       styleClass: styles.deleteBtn,
       src: ICON_DELETE,
       alt: ALT_TAG_ICON_DELETE,
+      onClick: onDeleteClick,
     },
   ];
 
   return (
     <div className="flex items-center">
       {actionButtons.map((actionButton) => (
-        <Button key={actionButton.id} className={actionButton.styleClass}>
+        <Button
+          key={actionButton.id}
+          className={actionButton.styleClass}
+          onClick={actionButton.onClick}
+        >
           <Image src={actionButton.src} alt={actionButton.alt} />
         </Button>
       ))}
     </div>
   );
 }
+
+ButtonGroup.propTypes = {
+  onDeleteClick: propTypes.func.isRequired,
+};
 
 export default ButtonGroup;
