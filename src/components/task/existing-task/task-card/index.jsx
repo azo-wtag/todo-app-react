e96ddb8f4aee189c707a6df788d348d9f242a91c
azo-wtag <ahm.zobyer@welldev.io>
2023-05-17 15:59:54 +0600

    refactor: task-card import order

--- src/components/task/existing-task/task-card/index.jsx
+++ src/components/task/existing-task/task-card/index.jsx
@@ -3,9 +3,9 @@ import propTypes from "prop-types";
 import dayjs from "dayjs";
 import styles from "components/task/existing-task/task-card/index.module.scss";
 import Button from "components/base/button";
+import ButtonGroup from "components/task/existing-task/button-group";
 import { TASK_DATE_FORMAT } from "utils/const";
 import { validateDayjsDate } from "utils/helper/validation";
-import ButtonGroup from "components/task/existing-task/button-group";
 
 function TaskCard({ createdAt, isCompleted, title }) {
   function formatDate(date) {
