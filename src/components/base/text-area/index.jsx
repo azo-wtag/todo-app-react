271b521549178a442796c211004eb0331cd724fb
azo-wtag <ahm.zobyer@welldev.io>
2023-05-18 14:25:57 +0600

    feat: add autofocus, value, onchange in text-area

--- src/components/base/text-area/index.jsx
+++ src/components/base/text-area/index.jsx
@@ -1,32 +1,46 @@
-import React, { forwardRef } from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { TEXTAREA_DEFAULT_ROW } from "utils/const";
 
-const TextArea = forwardRef(function TextArea(props, ref) {
-  const { errorMessage, numOfRows, name } = props;
+function TextArea(props) {
+  const { errorMessage, numOfRows, name, shouldAutoFocus, value, onChange } =
+    props;
+  const taskInputRef = useRef(null);
+
+  useEffect(() => {
+    if (shouldAutoFocus) {
+      taskInputRef.current.focus();
+    }
+  }, []);
 
   return (
     <>
       <textarea
         className="width-full"
-        rows={numOfRows}
-        ref={ref}
         name={name}
+        value={value}
+        rows={numOfRows}
+        onChange={onChange}
+        ref={taskInputRef}
       ></textarea>
       {errorMessage !== "" && <p>{errorMessage}</p>}
     </>
   );
-});
+}
 
 TextArea.propTypes = {
   errorMessage: PropTypes.string,
   numOfRows: PropTypes.number,
   name: PropTypes.string.isRequired,
+  shouldAutoFocus: PropTypes.bool,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 TextArea.defaultProps = {
   numOfRows: TEXTAREA_DEFAULT_ROW,
   errorMessage: "",
+  shouldAutoFocus: false,
 };
 
 export default TextArea;
