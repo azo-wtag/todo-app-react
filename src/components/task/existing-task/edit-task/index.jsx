9d1f021ddcaf501b5ee7c83a21fb43432260e4d3
azo-wtag <ahm.zobyer@welldev.io>
2023-05-18 10:06:26 +0600

    refactor: edit-task functionality without using form

--- src/components/task/existing-task/edit-task/index.jsx
+++ src/components/task/existing-task/edit-task/index.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import propTypes from "prop-types";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
 import { useDispatch } from "react-redux";
 import DOMPurify from "dompurify";
 import TextArea from "components/base/text-area";
@@ -9,90 +7,64 @@ import Button from "components/base/button";
 import Image from "components/base/image";
 import {
   TYPE_BUTTON,
-  TITLE_FIELD_NAME_ATTRIBUTE,
   ICON_CHECK,
-  ALT_CHECK_ICON_TAG,
   ICON_DELETE,
-  ALT_DELETE_ICON_TAG,
-  CUSTOM_ERROR_MESSAGE_TYPE,
-  TASK_TITLE_ERROR_MESSAGE,
-  FORM_VALIDATION_MODE_ONCHANGE,
+  ALT_TAG_ICON_DELETE,
+  ALT_TAG_ICON_CHECK,
+  ERROR_MESSAGE_TASK_TITLE,
+  ATTRIBUTE_TITLE,
 } from "utils/const";
-import { taskSchema } from "utils/schema";
 import { editTask, markAsDone } from "store/actions/todo";
 
 function EditTaskForm({ taskId, existingTitle, onDelete, onTaskEdit }) {
   const dispatch = useDispatch();
+  const taskInputRef = useRef(null);
+  const [errorMsg, setErrorMsg] = useState("");
 
   function titleSanitizer(title) {
     const sanitizedTitle = DOMPurify.sanitize(title);
     if (sanitizedTitle === "") {
-      setError(TITLE_FIELD_NAME_ATTRIBUTE, {
-        type: CUSTOM_ERROR_MESSAGE_TYPE,
-        message: TASK_TITLE_ERROR_MESSAGE,
-      });
-      setValue(TITLE_FIELD_NAME_ATTRIBUTE, sanitizedTitle);
+      setErrorMsg(ERROR_MESSAGE_TASK_TITLE);
+      taskInputRef.current.value = sanitizedTitle;
     }
 
     return sanitizedTitle;
   }
 
-  function updateTask(task) {
-    const title = titleSanitizer(task.title);
+  function updateTask(event) {
+    event.preventDefault();
+    const title = titleSanitizer(taskInputRef.current.value);
     if (title === "") return;
     dispatch(editTask({ taskId, title: title }));
-    setValue(TITLE_FIELD_NAME_ATTRIBUTE, null);
+    taskInputRef.current.value = null;
     onTaskEdit();
   }
 
-  function saveAsDone(task) {
-    const title = titleSanitizer(task.title);
+  function saveAsDone(event) {
+    event.preventDefault();
+    const title = titleSanitizer(taskInputRef.current.value);
     if (title === "") return;
     dispatch(editTask({ taskId, title: title }));
     dispatch(markAsDone(taskId));
-    setValue(TITLE_FIELD_NAME_ATTRIBUTE, null);
+    taskInputRef.current.value = null;
     onTaskEdit();
   }
 
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    setFocus,
-    setError,
-    formState: { errors },
-  } = useForm({
-    mode: FORM_VALIDATION_MODE_ONCHANGE,
-    resolver: yupResolver(taskSchema),
-  });
-
-  function handleEditTask(e) {
-    handleSubmit(updateTask)(e);
-  }
-
-  function handleSaveTask(e) {
-    handleSubmit(saveAsDone)(e);
-  }
-
   useEffect(() => {
-    setFocus(TITLE_FIELD_NAME_ATTRIBUTE);
-    setValue(TITLE_FIELD_NAME_ATTRIBUTE, existingTitle);
+    taskInputRef.current.focus();
+    taskInputRef.current.value = existingTitle;
   }, []);
 
   return (
     <form>
-      <TextArea
-        register={{ ...register(TITLE_FIELD_NAME_ATTRIBUTE) }}
-        error={errors.title}
-      />
-
+      <TextArea ref={taskInputRef} name={ATTRIBUTE_TITLE} errorMsg={errorMsg} />
       <div className="flex items-center">
-        <Button onClick={handleEditTask}>Save</Button>
-        <Button onClick={handleSaveTask}>
-          <Image src={ICON_CHECK} alt={ALT_CHECK_ICON_TAG} />
+        <Button onClick={updateTask}>Save</Button>
+        <Button onClick={saveAsDone}>
+          <Image src={ICON_CHECK} alt={ALT_TAG_ICON_CHECK} />
         </Button>
         <Button buttonType={TYPE_BUTTON} onClick={onDelete}>
-          <Image src={ICON_DELETE} alt={ALT_DELETE_ICON_TAG} />
+          <Image src={ICON_DELETE} alt={ALT_TAG_ICON_DELETE} />
         </Button>
       </div>
     </form>
