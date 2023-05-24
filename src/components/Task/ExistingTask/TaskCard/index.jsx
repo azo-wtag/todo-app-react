02df9c0e4259a24fe76b9c4519c4d6c5505dd79a
azo-wtag <ahm.zobyer@welldev.io>
2023-05-24 11:25:16 +0600

    feat: add done button click handler

--- src/components/Task/ExistingTask/TaskCard/index.jsx
+++ src/components/Task/ExistingTask/TaskCard/index.jsx
@@ -1,24 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useDispatch } from "react-redux";
+import classnames from "classnames";
 import ActionButtonGroup from "components/ActionButtonGroup";
+import CompletedDays from "components/Common/CompletedDays";
 import { formatDate } from "utils/helper";
-import { deleteTask } from "store/actions/todo";
+import { deleteTask, markAsDone } from "store/actions/todo";
 import styles from "components/Task/ExistingTask/TaskCard/index.module.scss";
 
-function TaskCard({ taskId, createdAt, title }) {
+function TaskCard({ taskId, createdAt, title, isCompleted, completedAt }) {
   const dispatch = useDispatch();
 
   function handleDeleteClick() {
     dispatch(deleteTask(taskId));
   }
 
+  function handleDoneClick() {
+    dispatch(markAsDone(taskId));
+  }
+
+  const taskHeaderClasses = classnames({ "text-line-through": isCompleted });
+
   return (
     <div className={styles.card}>
-      <h3>{title}</h3>
+      <h3 className={taskHeaderClasses}>{title}</h3>
       <p className={styles.date}>Created At: {formatDate(createdAt)}</p>
       <div className="flex justify-between">
-        <ActionButtonGroup onDeleteClick={handleDeleteClick} />
+        <ActionButtonGroup
+          isTaskCompleted={isCompleted}
+          onDeleteClick={handleDeleteClick}
+          onDoneClick={handleDoneClick}
+        />
+        {isCompleted && (
+          <CompletedDays endDate={completedAt} startDate={createdAt} />
+        )}
       </div>
     </div>
   );
