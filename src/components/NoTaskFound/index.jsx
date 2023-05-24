d30c255df3370adc5a4dd5c49313e742f4f8484d
azo-wtag <ahm.zobyer@welldev.io>
2023-05-24 09:15:45 +0600

    refactor: rename alt no task found const

--- src/components/NoTaskFound/index.jsx
+++ src/components/NoTaskFound/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "components/Common/Image";
 import {
-  ALT_TAG_NO_TASK_FOUND_ICON,
+  ALT_TAG_ICON_NO_TASK_FOUND,
   ICON_NO_TASK_FOUND,
 } from "utils/const/images";
 import styles from "components/NoTaskFound/index.module.scss";
@@ -9,7 +9,7 @@ import styles from "components/NoTaskFound/index.module.scss";
 function NoTaskFound() {
   return (
     <div className={`flex flex-col items-center ${styles.container}`}>
-      <Image src={ICON_NO_TASK_FOUND} alt={ALT_TAG_NO_TASK_FOUND_ICON} />
+      <Image src={ICON_NO_TASK_FOUND} alt={ALT_TAG_ICON_NO_TASK_FOUND} />
       <h4 className={styles.heading}>
         You didn’t add any task. Please, add one.
       </h4>
